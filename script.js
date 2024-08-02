1b2cecde8716ebdb8c719caad0beb64b9f691bd5
OlYeromin <ol.yeromin@gmail.com>
2024-08-02 14:43:20 +0300

    Create function to create a grid of squares

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function setSize(elementsSelected, dimension) {
     elementsSelected.setAttribute("width", `${elementSize}px`);
     elementsSelected.setAttribute("height", `${elementSize}px`);
 }
+
+function createGrid(dimension) {
+    for (let i = 0; i < dimension; i++) {
+        for (let j = 0; j < dimension; j++) {
+            appendSquare();
+        }
+    }
+    squares = document.querySelectorAll(".square");
+    setSize(squares, dimension);
+}
+
+document.addEventListener("load", createGrid(16));
